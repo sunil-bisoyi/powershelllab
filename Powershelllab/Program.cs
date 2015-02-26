626ef558e6e81559ccd8781f4ce2915c551c25e3
sunil-bisoyi <sunil.bisoyi@ignify.com>
2015-02-26 17:40:46 +0530

    project add

---
+++ Powershelllab/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;             // Windows PowerShell namespace.
+using System.Management.Automation.Runspaces;   // Windows PowerShell namespace
+namespace Powershelllab
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+            WSManConnectionInfo connectionInfo = new WSManConnectionInfo();
+            
+
+            connectionInfo.OperationTimeout = 4 * 60 * 1000; // 4 minutes.
+
+
+            connectionInfo.OpenTimeout = 1 * 60 * 1000; // 1 minute.
+
+
+            //foreach (var process in PowerShell.Create().AddCommand("Get-Process").Invoke())
+            //{
+            //    Console.WriteLine(process.Properties["ProcessName"].Value);
+            //} 
+
+
+
+
+            }
+
+  
+        }
+    }
+
+
